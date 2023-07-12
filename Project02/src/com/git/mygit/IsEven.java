036949416d7b2057be4045ef5ddc199bf3b9c249
Rajendra <rajendraks527@gmail.com>
2023-07-12 10:49:25 +0530

    first commit

---
+++ Project02/src/com/git/mygit/IsEven.java
@@ -0,0 +1,17 @@
+package com.git.mygit;
+
+import java.util.Scanner;
+
+public class IsEven {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0)
+			System.out.println("The number is even");
+		else
+			System.out.println("The number is odd");
+
+	}
+
+}
